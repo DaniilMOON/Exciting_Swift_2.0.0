b00b6f22106cc8f38f83508179b73d1338d70f3b
DaniilMOON <dan_osipov02@mail.ru>
2025-02-26 23:21:18 +0300

    Initial Commit

---
+++ Exciting_Swift_2.0.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exciting_Swift_2.0.0
+//
+//  Created by Даниил Осипов on 26.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
