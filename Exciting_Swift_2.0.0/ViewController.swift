20696f1488cce2b9ad36a8f8c687e0178b53836f
DaniilMOON <dan_osipov02@mail.ru>
2025-02-28 00:25:41 +0300

    star v2

--- Exciting_Swift_2.0.0/ViewController.swift
+++ Exciting_Swift_2.0.0/ViewController.swift
@@ -13,8 +13,8 @@ class ViewController: UIViewController {
     private var sliderValue = 20 {
         didSet {
             valueLabel.text = "\(sliderValue)%"
-            drawStar(alignment: .horizontal, star: horizontalStar)
-            drawStar(alignment: .vertical, star: verticalStar)
+            widthHighlightedStar.constant = horizontalStar.bounds.width * CGFloat(sliderValue) / 100
+            heightHighlightedStar.constant = verticalStar.bounds.height * CGFloat(sliderValue) / 100
         }
     }
     private let colors: [UIColor] = [.red, .gray]
@@ -31,12 +31,11 @@ class ViewController: UIViewController {
 
     private func configuration() {
         valueLabel.text = "\(sliderValue)%"
+        widthHighlightedStar.constant = horizontalStar.bounds.width * CGFloat(sliderValue) / 100
+        heightHighlightedStar.constant = verticalStar.bounds.height * CGFloat(sliderValue) / 100
 
         maskForStar(star: horizontalStar)
         maskForStar(star: verticalStar)
-
-        drawStar(alignment: .horizontal, star: horizontalStar)
-        drawStar(alignment: .vertical, star: verticalStar)
     }
 
     // MARK: Action
@@ -47,7 +46,9 @@ class ViewController: UIViewController {
 
     // MARK: Outlet
 
+    @IBOutlet weak var widthHighlightedStar: NSLayoutConstraint!
     @IBOutlet weak var horizontalStar: UIView!
+    @IBOutlet weak var heightHighlightedStar: NSLayoutConstraint!
     @IBOutlet weak var verticalStar: UIView!
     @IBOutlet weak var sliderView: UISlider!
     @IBOutlet weak var valueLabel: UILabel!
@@ -56,40 +57,6 @@ class ViewController: UIViewController {
 
     private enum Alignment {
         case horizontal, vertical
-    }
-
-    private func drawStar(alignment: Alignment, star: UIView) {
-        let (parts, colors) = switch alignment {
-        case .horizontal:
-            ([CGFloat(sliderValue) / 100, CGFloat(100 - sliderValue) / 100], colors)
-        case .vertical:
-            ([CGFloat(100 - sliderValue) / 100, CGFloat(sliderValue) / 100], colors.reversed())
-        }
-        var point: CGPoint = .zero
-
-        let renderer = UIGraphicsImageRenderer(size: horizontalStar.bounds.size)
-        let patternImage = renderer.image { context in
-            zip(colors, parts).forEach { color, part in
-                var size = self.horizontalStar.bounds.size
-                switch alignment {
-                case .horizontal:
-                    size.width *= part
-                case .vertical:
-                    size.height *= part
-                }
-
-                color.setFill()
-                context.fill(CGRect(origin: point, size: size))
-                switch alignment {
-                case .horizontal:
-                    point.x += horizontalStar.bounds.width * part
-                case .vertical:
-                    point.y += horizontalStar.bounds.width * part
-                }
-
-            }
-        }
-        star.backgroundColor = UIColor(patternImage: patternImage)
     }
 
     private func maskForStar(star: UIView) {
